c8a98a2eaf3c18d39d32cf5a1e85f5ccf5ca29f7
Graeme Porteous <graeme@rgbp.co.uk>
2021-06-25 16:33:24 +0100

    Patch outgoing email subject

--- lib/model_patches.rb
+++ lib/model_patches.rb
@@ -6,17 +6,13 @@
 # See http://stackoverflow.com/questions/7072758/plugin-not-reloading-in-development-mode
 #
 Rails.configuration.to_prepare do
+  InfoRequest.class_eval do
+    def email_subject_request(opts = {})
+      return super unless I18n.locale == :el
 
-  # Example of adding a default text to each message
-  # OutgoingMessage.class_eval do
-  #   # Add intro paragraph to new request template
-  #   def default_letter
-  #     # this line allows the default_letter text added by this
-  #     # method to be replaced by the value supplied by the API
-  #     # e.g. http://demo.alaveteli.org/new/tgq?default_letter=this+is+a+test
-  #     return @default_letter if @default_letter
-  #     return nil if self.message_type == 'followup'
-  #     "If you uncomment this line, this text will appear as default text in every message"
-  #   end
-  # end
+      html = opts.fetch(:html, true)
+      _('Αίτημα Κατάθεσης Εγγράφων - {{title}}',
+        title: (html ? title : title.html_safe))
+    end
+  end
 end
